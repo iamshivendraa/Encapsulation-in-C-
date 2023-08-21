a9dd5632d5ff4a69b2fb991f72c96147a1efe950
iamshivendraa <shivendracpj@gmail.com>
2023-08-21 19:48:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Hello
+{
+    class Program
+    {
+        private string name;
+        private string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program myObj = new Program();
+            myObj.Name = "Shivendra Kumar";
+            Console.WriteLine(myObj.Name);
+            
+
+        }
+    }
+}
+
